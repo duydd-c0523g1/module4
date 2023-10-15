3903c7917ede4ec1418e2a9f22dea618664a3b5c
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-10-15 18:15:04 +0700

    fix

--- SpringController/ss2_2/src/main/java/com/example/ss2_2/service/CalculatorService.java
+++ SpringController/ss2_2/src/main/java/com/example/ss2_2/service/CalculatorService.java
@@ -1,15 +1,21 @@
 package com.example.ss2_2.service;
 
-import com.example.ss2_2.repository.ICalculatorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService implements ICalculatorService {
-    @Autowired
-    private ICalculatorRepository calculatorRepositoryl;
     @Override
     public double calculate(double num1, double num2, String operator) {
-        return calculatorRepositoryl.calculate(num1, num2, operator);
+        switch (operator) {
+            case "subtraction":
+                return num1 - num2;
+            case "multiply":
+                return num1 * num2;
+            case "division":
+                return num1 / num2;
+            default:
+                return num1 + num2;
+        }
     }
 }
+
