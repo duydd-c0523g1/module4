c1d28444661c37d79ee2a24d6b751ef0abed21ba
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-10-15 18:38:55 +0700

    Update DictionaryRepository.java

--- SpringMVC_OverView/ss1_2/src/main/java/com/example/ss1_2/Repository/DictionaryRepository.java
+++ SpringMVC_OverView/ss1_2/src/main/java/com/example/ss1_2/Repository/DictionaryRepository.java
@@ -8,26 +8,22 @@ import java.util.Map;
 @Repository
 public class DictionaryRepository implements IDictionaryRepository {
     private static Map<String, String> wordMap = new HashMap<>();
+
     static {
-        wordMap.put("dog","Con chó");
-        wordMap.put("cat","Con mèo");
-        wordMap.put("apple","Quả táo");
-        wordMap.put("turtle","Con rùa");
-        wordMap.put("bottle","Cái bình");
-        wordMap.put("laptop","Máy tính xách tay");
-        wordMap.put("coffee","Cà phê");
-        wordMap.put("class","Lớp");
-        wordMap.put("glass","Tấm kính");
-        wordMap.put("ring","Chiếc nhẫn");
+        wordMap.put("dog", "Con chó");
+        wordMap.put("cat", "Con mèo");
+        wordMap.put("apple", "Quả táo");
+        wordMap.put("turtle", "Con rùa");
+        wordMap.put("bottle", "Cái bình");
+        wordMap.put("laptop", "Máy tính xách tay");
+        wordMap.put("coffee", "Cà phê");
+        wordMap.put("class", "Lớp");
+        wordMap.put("glass", "Tấm kính");
+        wordMap.put("ring", "Chiếc nhẫn");
     }
+
     @Override
     public String translate(String word) {
-        if (word != null) {
-            String translation = wordMap.get(word.toLowerCase());
-            if (translation != null) {
-                return translation;
-            }
-        }
-        return "Từ này chưa có trong từ điển";
+        return word != null ? wordMap.getOrDefault(word.toLowerCase(), "Từ này chưa có trong từ điển") : "Từ này chưa có trong từ điển";
     }
 }
